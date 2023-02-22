2f6a87883ca6bf722d31a5566a2b37443664e0d6
Xiaolong "Bruce" Liu <liuxiaolong125@gmail.com>
2023-02-22 13:13:14 +0800

    adds `update` and `publish` funtions

--- src/zenodopy/zenodopy.py
+++ src/zenodopy/zenodopy.py
@@ -451,11 +451,12 @@ class Client(object):
         else:
             return None
 
-    def upload_file(self, file_path=None):
+    def upload_file(self, file_path=None, publish=False):
         """upload a file to a project
 
         Args:
-            filename (str): name of the file to upload
+            file_path (str): name of the file to upload
+            publish (bool): whether implemente publish action or not
         """
         if file_path is None:
             print("You need to supply a path")
@@ -473,8 +474,11 @@ class Client(object):
                                  data=fp,)
 
                 print(f"{file_path} successfully uploaded!") if r.ok else print("Oh no! something went wrong")
+            
+            if publish:
+                return self.publish()
 
-    def upload_zip(self, source_dir=None, output_file=None):
+    def upload_zip(self, source_dir=None, output_file=None, publish=False):
         """upload a directory to a project as zip
 
         This will: 
@@ -486,6 +490,7 @@ class Client(object):
             source_dir (str): path to directory to tar
             output_file (str): name of output file (optional)
                 defaults to using the source_dir name as output_file
+            publish (bool): whether implemente publish action or not, argument for `upload_file`
         """
         # make sure source directory exists
         source_dir = os.path.expanduser(source_dir)
@@ -526,12 +531,12 @@ class Client(object):
                 make_zipfile(source_dir, zipf)
 
         # upload the file
-        self.upload_file(file_path=output_file)
+        self.upload_file(file_path=output_file, publish=publish)
 
         # remove tar file after uploading it
         os.remove(output_file)
 
-    def upload_tar(self, source_dir=None, output_file=None):
+    def upload_tar(self, source_dir=None, output_file=None, publish=False):
         """upload a directory to a project
 
         This will: 
@@ -543,6 +548,7 @@ class Client(object):
             source_dir (str): path to directory to tar
             output_file (str): name of output file (optional)
                 defaults to using the source_dir name as output_file
+            publish (bool): whether implemente publish action or not, argument for `upload_file`
         """
         # output_file = './tmp/tarTest.tar.gz'
         # source_dir = '/Users/gloege/test'
@@ -584,10 +590,53 @@ class Client(object):
             make_tarfile(output_file=output_file, source_dir=source_dir)
 
         # upload the file
-        self.upload_file(file_path=output_file)
+        self.upload_file(file_path=output_file, publish=publish)
 
         # remove tar file after uploading it
         os.remove(output_file)
+
+    def update(self, source=None, output_file=None, publish=False):
+        """update an existed record
+
+        Args:
+            source (str): path to directory or file to upload
+            output_file (str): name of output file (optional)
+                defaults to using the source_dir name as output_file
+            publish (bool): whether implemente publish action or not, argument for `upload_file`
+        """
+        # create a draft deposition
+        url_action = self._get_depositions_by_id(self.deposition_id)['links']['newversion']
+        r = requests.post(url_action, auth=self._bearer_auth)
+        r.raise_for_status()
+
+        # parse current project to the draft deposition
+        new_dep_id = r.json()['links']['latest_draft'].split('/')[-1]
+        self.set_project(new_dep_id)
+
+        # invoke upload funcions
+        if not source:
+            print("You need to supply a path")
+        
+        if Path(source).exists():
+            if Path(source).is_file():
+                self.upload_file(source, publish=publish)
+            elif Path(source).is_dir():
+                if not output_file:
+                    self.upload_zip(source, publish=publish)
+                elif '.zip' in ''.join(Path(output_file).suffixes).lower():
+                    self.upload_zip(source, output_file, publish=publish)
+                elif '.tar.gz' in ''.join(Path(output_file).suffixes).lower():
+                    self.upload_tar(source, output_file, publish=publish)
+        else:
+            raise FileNotFoundError(f"{source_dir} does not exist")
+        
+    def publish(self):
+        """ publish a record
+        """
+        url_action = self._get_depositions_by_id(self.deposition_id)['links']['publish']
+        r = requests.post(url_action, auth=self._bearer_auth)
+        r.raise_for_status()
+        return r
 
     def download_file(self, filename=None, dst_path=None):
         """download a file from project
